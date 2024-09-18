985f2c35c64c1336f930cfabf95c5feeb52b17cd
JesrielAlegado <alegadojesriel@gmail.com>
2024-09-18 16:44:08 +0800

    Prelim

---
+++ src/alegado/bsit/pkg2e/ALEGADOBSIT2e.java
@@ -0,0 +1,98 @@
+
+package alegado.bsit.pkg2e;
+
+public class ALEGADOBSIT2e {
+
+    
+    
+    package Booking id{
+    
+    
+    public class Main{
+    
+        
+        
+        public static void main (String[]orgs){
+            
+           Execute input = new Execute (); 
+           input.getBooking();
+           
+        }      
+    }
+
+      
+        public class Booking{
+            
+        int id;
+        int name;
+        int room;
+        int num;
+        int date; 
+        
+        
+        private int status;
+        
+        public void AddBooking(int id, int name, int room, int num, int date){
+            
+            this.id = id;
+            this.name = name;
+            this.room = room;
+            this.num = num;
+            int.price = price;
+        }
+        public void viewBookingCheck(int id, double lomi){
+            string guestName;            
+            int name[100];
+            double name;
+            
+            public void addGuest(ind id, int name, int room,int num, int price){
+                double price{
+                this.id = id;
+                this.name = name;
+                this.num = num;
+                this.
+            
+            
+            
+            
+           
+            
+            
+            
+            
+          
+               
+               
+           
+            
+              
+                
+                
+        
+       
+        
+            
+            
+            
+          
+            
+            
+            
+       
+        
+    
+    
+   
+    
+    
+          
+        
+      
+      
+      
+        
+        
+        }
+    }
+    
+}
